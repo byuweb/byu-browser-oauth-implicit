e71922919fde63d066b9631c3689083fb28dfffc
Glen Sawyer <github@snelg.com>
2019-07-25 14:40:01 -0600

    Remove tests for IE support

--- karma.conf.js
+++ karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
     frameworks: ['mocha', 'chai', 'detectBrowsers', 'sinon'],
- 
+
     // list of files / patterns to load in the browser
     files: [
       {
@@ -60,6 +60,13 @@ module.exports = function (config) {
         if (chrome > -1) {
           result.splice(chrome, 1, 'ChromeHeadless')
         }
+
+        // We no longer need to support IE
+        const IE = available.indexOf('IE');
+        if (IE > -1) {
+          result.splice(IE, 1)
+        }
+
         return result;
       }
     },
